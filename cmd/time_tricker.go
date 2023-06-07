f25c42ca1ee929bef802bf13263f2d0865344e4c
MarcoSteinke <marco.steinke@innoq.com>
2023-06-07 15:56:37 +0200

    add basic logic to time_tricker.go

--- cmd/time_tricker.go
+++ cmd/time_tricker.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	args := os.Args
+
+	// TODO: refactor if-else-hell
+	if len(args) == 3 {
+		// start a timer
+		fmt.Println("starting timer " + args[2])
+	} else if len(args) == 2 {
+		// stop the running timer
+		if args[1] == "stop" {
+			fmt.Println("stopping timer")
+		} else {
+			fmt.Println("Wrong syntax, did you want to do something else?")
+			fmt.Println("Use \"time stop\" to stop the currently running timer.")
+		}
+	} else {
+		fmt.Println("Not the right format")
+		fmt.Println("time start <name> to start a timer")
+		fmt.Println("time stop to stop the running timer")
+	}
+
 }
